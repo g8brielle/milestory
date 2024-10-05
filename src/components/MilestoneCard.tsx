00136544e032f98fa08e0f11d06535c47648f9ed
Gabrielle Christensen <gchristensen@mines.edu>
2024-10-05 16:25:58 -0600

    Date now updates when you check something off

--- src/components/MilestoneCard.tsx
+++ src/components/MilestoneCard.tsx
@@ -1,21 +1,33 @@
-import React, { ReactNode } from 'react';
+import React, { useState, useEffect } from 'react';
 import MilestoneCheckbox from './MilestoneCheckbox';
 
 interface MilestoneCardProps {
     className?: string; 
     milestone: string;
-    date: string;
     note: string;
 }
 
-const MilestoneCard : React.FC<MilestoneCardProps> = ({ className , milestone, date, note }) => {
+const MilestoneCard : React.FC<MilestoneCardProps> = ({ className , milestone, note}) => {
+  const [date, setDate] = useState<string>('');
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const updateDate = () => {
+    if (isChecked) {
+      setDate('');
+    } else {
+      const currentDate = new Date().toLocaleDateString();
+      setDate(currentDate);
+    }
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div>
       <div className='d-flex align-items-center justify-content-center gap-3'>
-        <MilestoneCheckbox />
+        <MilestoneCheckbox onCheck={updateDate}/>
         <h1>Milestone: {milestone}</h1>
       </div>
-      <p>Acheived On:<br></br>{date}</p>
+      <p>Acheived On:<br></br>{date || "Not yet achieved"}</p>
       <p>{note}</p>
     </div>
   );
--- src/components/MilestoneCheckbox.tsx
+++ src/components/MilestoneCheckbox.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-function MilestoneCheckbox() {
+interface MilestoneCheckboxProps {
+  onCheck: () => void;
+}
+
+function MilestoneCheckbox({onCheck}: MilestoneCheckboxProps) {
   const [isChecked, setIsChecked] = useState(false);
   const circleStyle = {
-    width: '40px',
-    height: '40px',
-    borderRadius: '50%', 
-    border: 'solid 3px var(--navy-blue)',
-    cursor: 'pointer',
-    backgroundColor: isChecked ? 'var(--light-green)' : 'white'
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+    border: "solid 3px var(--navy-blue)",
+    cursor: "pointer",
+    backgroundColor: isChecked ? "var(--light-green)" : "white",
   };
 
   const handleCheckboxChange = () => {
-    setIsChecked(!isChecked); 
+    setIsChecked(!isChecked);
+    onCheck();
   };
 
   return (
