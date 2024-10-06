61f997070a29bd0d248c2f64e439f62115ed1dd1
Gabrielle Christensen <gchristensen@mines.edu>
2024-10-06 13:21:03 -0600

    Added view reminders butt

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import '../styles.css';
 import MilestoneLog from './MilestoneLog';
+import { Modal } from 'react-bootstrap';
 
 const NavBar = () => {
     const [isPopupOpen, setPopupOpen] = useState(false);
@@ -12,26 +13,46 @@ const NavBar = () => {
         setPopupOpen(false);
     };
 
-  return (
-    <div>
-        <nav>
-            <div className='navbar-container'>
-                <div className='row align-items-center w-100'>
-                    <div className='col-2'>
-                        <div className="navbar-logo">
-                            <a href="/">Milestory</a>
-                        </div>
-                    </div>
-                    <div className='col-10'>
-                        <div className="navbar-links">
-                            <a><p>view reminders</p></a>
-                        </div>
-                    </div>
+    return (
+        <div>
+          <nav>
+            <div className="navbar-container">
+              <div className="row align-items-center w-100">
+                <div className="col-2">
+                  <div className="navbar-logo">
+                    <a href="/">Milestory</a>
+                  </div>
                 </div>
+                <div className="col-10">
+                  <div className="navbar-links">
+                    <a onClick={handleOpenPopup}>
+                      <p>view reminders</p>
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-        </nav>
-    </div>
-  );
-};
-
-export default NavBar;
+          </nav>
+    
+          {/* Modal for Reminders */}
+          <Modal show={isPopupOpen} onHide={handleClosePopup}>
+            <Modal.Header closeButton>
+              <Modal.Title>Appointment Reminders</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <ul>
+                <li><strong>2 months:</strong> First vaccination</li>
+                <li><strong>4 months:</strong> Second vaccination</li>
+                <li><strong>6 months:</strong> Health check-up</li>
+                {/* Add more reminders as needed */}
+              </ul>
+            </Modal.Body>
+            <Modal.Footer>
+              <button onClick={handleClosePopup}>Close</button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      );
+    };
+    
+    export default NavBar;
